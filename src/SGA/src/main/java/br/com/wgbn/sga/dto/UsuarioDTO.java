f6dcc23b43f0b35fd3c098d6f495a477a56f0bb4
MrMenezes <sr.tama@outlook.com>
2015-10-08 21:11:50 -0300

    TipoUsuarioDTO

--- src/SGA/src/main/java/br/com/wgbn/sga/dto/UsuarioDTO.java
+++ src/SGA/src/main/java/br/com/wgbn/sga/dto/UsuarioDTO.java
@@ -6,4 +6,55 @@ package br.com.wgbn.sga.dto;
  */
 public class UsuarioDTO {
     
+
+    private int id;
+    
+    private String nome;
+    
+    private TipoUsuarioDTO tipo;
+    
+    private String email;
+
+    public UsuarioDTO() {
+    }
+
+    public UsuarioDTO(int id, String nome, TipoUsuarioDTO tipo, String email) {
+        this.id = id;
+        this.nome = nome;
+        this.tipo = tipo;
+        this.email = email;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public TipoUsuarioDTO getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(TipoUsuarioDTO tipo) {
+        this.tipo = tipo;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    
 }
