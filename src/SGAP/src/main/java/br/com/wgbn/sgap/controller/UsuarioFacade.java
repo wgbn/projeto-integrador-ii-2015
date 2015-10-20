722e2d50ef5251cd8a68f858e22c89c0146dfafd
Walter Gandarella <walter.wgbn@gmail.com>
2015-10-20 09:22:25 -0300

    projeto feito no IntelliJ IDEA

---
+++ src/SGAP/src/main/java/br/com/wgbn/sgap/controller/UsuarioFacade.java
@@ -0,0 +1,17 @@
+package br.com.wgbn.sgap.controller;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+
+/**
+ * Created by Walter Gandarella on 20/10/15.
+ */
+@ManagedBean
+@SessionScoped
+public class UsuarioFacade {
+
+    public UsuarioFacade {
+
+    }
+
+}
