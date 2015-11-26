5137bf6144cb86e75fa33bd22d405dd3a8cc2937
Walter Gandarella <walter.wgbn@gmail.com>
2015-11-26 16:35:36 -0300

    out

---
+++ src/SGAP/out/artifacts/SGAP_war_exploded/template/js/app.js
@@ -0,0 +1,56 @@
+$(document).ready(function(){
+
+    // remove css do primefaces
+    $.each($('head').find('link'), function(_key,_val){
+        if ($(this).attr('href').search(new RegExp("primefaces.css.xhtml","g")) > -1)
+            $(this).remove();
+        if ($(this).attr('href').search(new RegExp("layout.css.xhtml","g")) > -1)
+            $(this).remove();
+    });
+    // remove js do primefaces
+    $.each($('head').find('script'), function(_key,_val){
+        if ($(this).attr('src')) {
+            if ($(this).attr('src').search(new RegExp("primefaces.js.xhtml", "g")) > -1)
+                $(this).remove();
+            if ($(this).attr('src').search(new RegExp("layout.js.xhtml", "g")) > -1)
+                $(this).remove();
+            if ($(this).attr('src').search(new RegExp("jquery.js.xhtml","g")) > -1)
+                $(this).remove();
+            if ($(this).attr('src').search(new RegExp("jquery-plugins.js.xhtml","g")) > -1)
+                $(this).remove();
+        }
+    });
+
+    // remove todas as classes do primefaces
+    $.each($('.app').find('*'), function(){
+        if ($(this).attr('class')){
+            var arr = $(this).attr('class').split(' ');
+            var cls = '';
+            arr.forEach(function(_val){
+                if (_val.substr(0,2) != 'ui')
+                    cls += _val+' ';
+            });
+            cls = cls.trim();
+
+            if (cls.length > 0)
+                $(this).attr('class', cls);
+            else
+                $(this).removeAttr('class');
+        }
+    });
+
+    // posições absolutas
+    $('.esquerda').height($(window).height());
+    $('.topo').width($(window).width() - $('.esquerda').width());
+
+    // acoes menu esquerda
+    $('.esquerda .menu .opcoes ul li a.sup').click(function(e){
+        e.preventDefault();
+        var _id = $(this).attr('id');
+        $('#sub'+_id).slideToggle(150);
+        if ($('#'+_id+' img')[0].classList.length > 1)
+            $('#'+_id+' img').removeClass('seta-baixo');
+        else
+            $('#'+_id+' img').addClass('seta-baixo');
+    });
+});
